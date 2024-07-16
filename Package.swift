642501a53d43713e7b439235fee630929869422a
astinz <28899947+astinz@users.noreply.github.com>
2024-07-16 16:52:09 +0300

    update artifact url...

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
    targets: [
       .binaryTarget(
          name: "AptosKit",
-         url: "",
+         url: "https://github.com/mcxross/swift-aptos/releases/download/v0.1.0-beta/AptosKit.xcframework.zip",
          checksum:"ba2f201a463a57181508b5e8a1dcac11553d48a4fb3a123015dad0b7babf6814")
    ]
 )
